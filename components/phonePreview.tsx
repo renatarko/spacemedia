fa9b3e1fcc7fe45b558ad29c3efdb538b68b2abe
Renata Karonlina <renatakarolinarko@gmail.com>
2023-12-08 15:20:56 -0400

    fix

--- components/phonePreview.tsx
+++ components/phonePreview.tsx
@@ -2,19 +2,22 @@
 
 import { usePreview } from "@/context/preview";
 import { Link as LinkDB } from "@/types/types";
+import { DocumentData } from "firebase/firestore";
 import Image from "next/image";
 import Link from "./link";
 import LinkName from "./linkName";
 
+type User = {
+  name: string;
+  avatar: string;
+  linkName: string;
+  background?: string;
+  career?: string;
+  links: LinkDB[];
+};
+
 type PhonePreviewProps = {
-  data: {
-    name: string;
-    avatar: string;
-    linkName: string;
-    background?: string;
-    career?: string;
-    links: LinkDB[];
-  };
+  data: User | DocumentData | undefined;
 };
 
 export default function PhonePreview({ data }: PhonePreviewProps) {
@@ -22,15 +25,15 @@ export default function PhonePreview({ data }: PhonePreviewProps) {
 
   return (
     <div className="h-full px-12 flex flex-col items-center  w-full ">
-      <LinkName linkNameSaved={data.linkName} />
+      <LinkName linkNameSaved={data?.linkName} />
 
       <aside className="px-6 overflow-x-hidden mt-20 relative pb-8 h-[44rem] overflow-y-auto border-[12px] sm:border-[12px] lg:w-[75%] w-full flex flex-col items-center rounded-2xl border-gray-700">
         <div
           className={`absolute top-0 bottom-0 left-0 right-0 border-4 border-gray-600 z-[-1]`}
-          style={{ background: data.background }}
+          style={{ background: data?.background }}
         />
         <Image
-          src={data.avatar}
+          src={data?.avatar}
           alt={`image`}
           className="w-24 h-24 rounded-full border-4 border-white mt-8 z-10 shadow-lg"
           width={300}
@@ -46,7 +49,7 @@ export default function PhonePreview({ data }: PhonePreviewProps) {
         </div>
 
         <ul className="flex flex-col gap-4 mt-8 w-full">
-          {data.links.map((link, i) => (
+          {data?.links.map((link: any, i: any) => (
             <Link
               key={i}
               url={link.url}

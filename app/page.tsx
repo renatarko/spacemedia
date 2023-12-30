90db49320eb84e75986e0a0dd77794804e2556b4
Renata Karonlina <renatakarolinarko@gmail.com>
2023-12-30 15:39:09 -0400

    added tuilt and swiper to create example to cel phone animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
+"use client";
+
 import Base from "@/components/base";
 import Container from "@/components/container";
 import { Instagram, Link2Icon, Youtube } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Tilt } from "react-tilt";
+import { Autoplay, EffectFlip } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const medias = ["Web Design", "Portfolio", "Talk to me", "Instagram"];
+import { defaultOptions, homeImagesCel } from "@/functions/constant";
+import "swiper/css";
+import "swiper/css/effect-flip";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default function Home() {
   return (
     <Base>
       <Container>
-        <section className="flex flex-col sm:flex-row justify-center sm:justify-between pb-28 mt-12 sm:mt-0">
+        <section className="flex flex-col sm:flex-row gap-4 justify-center sm:justify-between pb-28 mt-20 sm:mt-0">
           <div className="sm:mt-16 md:w-[50%] sm:w-[80%] text-center sm:text-start">
             <h1 className="text-xl font-bold text-blue-600">media space</h1>
             <h2 className="sm:text-5xl text-3xl mt-6 font-bold sm:leading-normal mb-12">
@@ -29,47 +38,120 @@ export default function Home() {
             </Link>
           </div>
 
-          <aside className="sm:w-[40%] w-full flex justify-center items-center mt-24 sm:mt-12 relative">
-            <div className="w-full max-w-max  self-center h-full flex bg-gradient-to-l shadow-xl shadow-cyan-200 from-blue-600 to-blue-800 rounded-2xl items-center flex-col">
-              <div className="w-24 h-24 overflow-auto rounded-full sm:mt-16 mt-8 overflow-hidden">
-                <Image
-                  src={"/imageRe.png"}
-                  alt="image"
-                  width={150}
-                  height={150}
-                  className=""
-                />
-              </div>
+          <div className="max-w-[17rem] w-full mt-20 sm:mt-12 self-center ">
+            <Tilt options={defaultOptions}>
+              <Swiper
+                modules={[EffectFlip, Autoplay]}
+                autoplay={{
+                  delay: 7000,
+                  disableOnInteraction: false,
+                }}
+                loop={true}
+                effect="flip"
+                slidesPerView={1}
+                spaceBetween={80}
+                grabCursor={true}
+              >
+                {homeImagesCel.map((item, i) => (
+                  <SwiperSlide key={i}>
+                    <div
+                      className={`w-full self-center h-full flex bg-gradient-to-l shadow-xl rounded-2xl items-center flex-col ${item.background.first} ${item.background.second} ${item.background.shadow}`}
+                    >
+                      <div className="w-24 h-24 rounded-full sm:mt-16 mt-8 overflow-hidden">
+                        <Image
+                          src={item.image}
+                          alt="image"
+                          width={150}
+                          height={150}
+                          className=""
+                        />
+                      </div>
 
-              <div className="mt-8 text-white px-8 md:px-12 text-center">
-                <h1 className="text-xl font-bold">Renata Karolina</h1>
-                <h2>Software Developer</h2>
-              </div>
+                      <div className="mt-8 text-white px-8 md:px-12 text-center">
+                        <h1 className="text-xl font-bold drop-shadow-lg">
+                          {item.title}
+                        </h1>
+                        <h2 className="drop-shadow-lg">{item.career}</h2>
+                      </div>
 
-              <ul className="w-full px-4 mt-6 pb-10">
-                {medias.map((item, i) => (
-                  <li
-                    key={i}
-                    className="w-full mt-2 bg-blue-900 p-2 rounded-full text-center text-white"
-                  >
-                    {item}
-                  </li>
+                      <ul className="w-full px-4 mt-6 pb-10">
+                        {item.buttons.link.map((link, i) => (
+                          <li
+                            key={i}
+                            className={`w-full drop-shadow-lg mt-2 p-2 rounded-full text-center font-bold ${item.buttons.color} ${item.buttons.background}`}
+                          >
+                            {link}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+
+                    <div
+                      className={`absolute left-16 bottom-[-2rem] flex gap-3  p-2 rounded-lg shadow-xl ${item.icons.containerBg}`}
+                    >
+                      <span
+                        className={`p-3 rounded-full ${item.icons.background} ${item.icons.color}`}
+                      >
+                        <Instagram />
+                      </span>
+                      <span
+                        className={`p-3 rounded-full ${item.icons.background} ${item.icons.color}`}
+                      >
+                        <Youtube />
+                      </span>
+                      <span
+                        className={`p-3 rounded-full ${item.icons.background} ${item.icons.color}`}
+                      >
+                        <Link2Icon />
+                      </span>
+                    </div>
+                  </SwiperSlide>
                 ))}
-              </ul>
-            </div>
 
-            <div className="absolute right-12 bottom-[-2rem] flex gap-3 bg-yellow-500 p-2 rounded-lg shadow-xl">
-              <span className="p-3 bg-white rounded-full text-yellow-700">
-                <Instagram />
-              </span>
-              <span className="p-3 bg-white rounded-full text-yellow-700">
-                <Youtube />
-              </span>
-              <span className="p-3 bg-white rounded-full text-yellow-700">
-                <Link2Icon />
-              </span>
-            </div>
-          </aside>
+                {/* <SwiperSlide>
+                  <div className="w-full max-w-max  self-center h-full flex bg-gradient-to-l shadow-xl shadow-green-200 from-green-600 to-green-900 rounded-2xl items-center flex-col">
+                    <div className="w-24 h-24 rounded-full sm:mt-16 mt-8 overflow-hidden">
+                      <Image
+                        src={"/imageRe.png"}
+                        alt="image"
+                        width={150}
+                        height={150}
+                        className=""
+                      />
+                    </div>
+
+                    <div className="mt-8 text-white px-8 md:px-20 text-center">
+                      <h1 className="text-xl font-bold">Fitness Air</h1>
+                      <h2>Gympass</h2>
+                    </div>
+
+                    <ul className="w-full px-4 mt-6 pb-10">
+                      {medias.map((item, i) => (
+                        <li
+                          key={i}
+                          className="w-full mt-2 bg-gray-300 p-2 rounded-full text-center text-green-900 font-semibold"
+                        >
+                          {item}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  <div className="absolute left-16 bottom-[-2rem] flex gap-3 bg-gray-900 p-2 rounded-lg shadow-xl">
+                    <span className="p-3 bg-green-500 rounded-full text-black">
+                      <Instagram />
+                    </span>
+                    <span className="p-3 bg-green-500 rounded-full text-black">
+                      <Youtube />
+                    </span>
+                    <span className="p-3 bg-green-500 rounded-full text-black">
+                      <Link2Icon />
+                    </span>
+                  </div>
+                </SwiperSlide> */}
+              </Swiper>
+            </Tilt>
+          </div>
         </section>
       </Container>
     </Base>
--- functions/constant.ts
+++ functions/constant.ts
@@ -11,3 +11,81 @@ export const mediasType = [
   { name: "Email", path: "mailto:" },
   { name: "Phone", path: "tel:" },
 ];
+
+export const defaultOptions = {
+  reverse: false,
+  max: 35,
+  perspective: 1000,
+  scale: 1.05,
+  speed: 2000,
+  transition: true,
+  axis: null,
+  reset: true,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+};
+
+export const homeImagesCel = [
+  {
+    background: {
+      first: "from-blue-600",
+      second: "to-blue-800",
+      shadow: "shadow-cyan-200",
+    },
+    image: "/imageRe.png",
+    title: "Renata Karolina",
+    career: "Web Developer",
+    nickname: "",
+    buttons: {
+      background: "bg-blue-900",
+      color: "text-white",
+      link: ["Web Design", "Portfolio", "Talk to me", "Instagram"],
+    },
+    icons: {
+      containerBg: "bg-yellow-500",
+      background: "bg-white",
+      color: "text-yellow-700",
+    },
+  },
+  {
+    background: {
+      first: "from-green-700",
+      second: "to-green-900",
+      shadow: "shadow-green-300",
+    },
+    image: "/bodyform.png",
+    title: "Body Form",
+    career: "Bodybuilding Gym",
+    nickname: "text-green-900",
+    buttons: {
+      background: "bg-gray-300",
+      color: "",
+      link: ["Instagram", "App", "Localization", "WhatsApp"],
+    },
+    icons: {
+      containerBg: "bg-white/20 backdrop-blur-sm ",
+      background: "bg-green-800",
+      color: "text-white",
+    },
+  },
+  {
+    background: {
+      first: "from-yellow-950",
+      second: "to-yellow-700",
+      shadow: "shadow-amber-300",
+    },
+    image: "/woman.jpg",
+    title: "Julia Saith",
+    career: "Interior Architect",
+    nickname: "",
+    buttons: {
+      background: "bg-amber-100",
+      color: "text-yellow-950",
+      link: ["Instagram", "Projects", "Blog and Books", "Contact"],
+    },
+    icons: {
+      containerBg: "bg-white/20 backdrop-blur-sm ",
+      background: "bg-yellow-700",
+      color: "text-white",
+    },
+  },
+];

434a116ed3fec2eaf492105da232747a1f12b4b7
Renata Karonlina <renatakarolinarko@gmail.com>
2023-12-30 16:56:24 -0400

    improving button when is layer small

--- components/button.tsx
+++ components/button.tsx
@@ -22,7 +22,7 @@ export default function Button({
       <Link
         href={path!}
         className={`py-2 disabled:bg-blue-100 disabled:pointer-events-none disabled:cursor-not-allowed cursor-pointer font-semibold px-5 text-lg text-black bg-blue-400 flex items-center gap-2 rounded-full relative overflow-hidden left-0 right-0 before:z-0 hover:before:scale-[4.8] before:duration-200 before:w-12 before:h-12 before:rounded-full before:absolute before:scale-0 before:bg-blue-800 before:opacity-0 hover:before:opacity-100  hover:text-blue-500 ${
-          minWidth && "max-w-max"
+          minWidth && "sm:max-w-max w-full justify-center"
         }`}
       >
         <span className="z-10">{children}</span>
